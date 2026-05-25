b9be70900c5eba4390be9513e96069b55934c4c6
cebarboza <ce.develop@pm.me>
2026-05-25 17:18:40 +0200

    Updating documentation and adding github rcmdcheck

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# studyGenerics 0.1.0
+
+* Initial CRAN submission.
--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(StudyToolBelt)
+library(studyGenerics)
 
-test_check("StudyToolBelt")
+test_check("studyGenerics")
